fb1092ff6cdee08d720c452185142580c0a43fa8
Igor Shelopaev <igorshelopaev@gmail.com>
2019-09-26 13:07:24 +0300

    Update Segment.java

--- pickerfx/src/main/java/com/dlsc/pickerfx/Segment.java
+++ pickerfx/src/main/java/com/dlsc/pickerfx/Segment.java
@@ -64,6 +64,8 @@ public class Segment<T, S> extends Control {
 
         setCellFactory(p -> new SegmentCell<>());
 
+        this.readOnlyProperty().bind(picker.readOnlyProperty());
+        
         if (items != null) {
             getItems().setAll(items);
         }
